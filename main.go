acdf6d31aace6e81183c91347fb6a9ce619ce0b9
dnguyen86 <dnguyen.etudes@gmail.com>
2017-06-20 17:14:26 +0200

    up

--- main.go
+++ main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"encoding/json"
 
 	"github.com/gin-gonic/gin"
 )
 
+func getFbPhoto() {
+	// url_result => get "data" => "url"
+	url := "http://graph.facebook.com/899295334/picture?type=large&redirect=false"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return
+	}
+	defer resp.Body.Close()
+	type Message struct {
+		is_silhouette, url string
+	}
+	var record Message
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		log.Println(err)
+	}
+	fmt.Println(record)
+	fmt.Println()
+}
 func main() {
+	getFbPhoto()
 	port := os.Getenv("PORT")
 
 	if port == "" {
