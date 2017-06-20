3d5043dbb6953ae6b8ca4f2ce10038a2f3fd07f8
dnguyen86 <dnguyen.etudes@gmail.com>
2017-06-20 11:32:12 +0200

    up

--- web.go
+++ web.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/", index)
+    http.HandleFunc("/", indexHandler)
     fmt.Println("listening...")
     err := http.ListenAndServe(":8080", nil)
     if err != nil {
@@ -16,4 +16,14 @@ func main() {
 
 func index(res http.ResponseWriter, req *http.Request) {
     fmt.Fprintln(res, "index page")
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    t, _ := template.ParseFiles("edit.html")
+    t.Execute(w, p)
 }
